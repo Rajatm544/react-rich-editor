55447ea3fc07c72368e9875e101b59569a10cb0b
Rajatm544 <rajatm544@gmail.com>
2022-03-27 18:28:14 +0530

    add a main container for the rich editor and render in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import Navbar from './components/Navbar';
+import MainContainer from './components/MainContainer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
+			<MainContainer />
 		</div>
 	);
 }
