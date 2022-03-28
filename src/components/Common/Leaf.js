add8282d636c731c61a32f2325a7b3590ee8c150
Rajatm544 <rajatm544@gmail.com>
2022-03-28 19:34:18 +0530

    handle all Slate Leaf options for rich editor

---
+++ src/components/Common/Leaf.js
@@ -0,0 +1,23 @@
+// Slate Leaf to handle style changes on single characters
+
+const Leaf = ({ attributes, children, leaf }) => {
+	if (leaf.bold) {
+		children = <strong>{children}</strong>;
+	}
+
+	if (leaf.code) {
+		children = <code>{children}</code>;
+	}
+
+	if (leaf.italic) {
+		children = <em>{children}</em>;
+	}
+
+	if (leaf.underline) {
+		children = <u>{children}</u>;
+	}
+
+	return <span {...attributes}>{children}</span>;
+};
+
+export default Leaf;
