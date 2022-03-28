8ab221e3348ea6a8bd4c1f1b868424eef6c03234
Rajatm544 <rajatm544@gmail.com>
2022-03-28 22:00:16 +0530

    update Leaf file to handle tag highlighting

--- src/components/Common/Leaf.js
+++ src/components/Common/Leaf.js
@@ -1,5 +1,13 @@
-// Slate Leaf to handle style changes on single characters
+import React from 'react';
+import {
+	assignedColors,
+	allColors,
+	getSelectionCoords,
+	colorTags,
+	placeDiv,
+} from '../Highlight/highlightUtilFunctions';
 
+// Slate Leaf to handle style changes on single characters
 const Leaf = ({ attributes, children, leaf }) => {
 	if (leaf.bold) {
 		children = <strong>{children}</strong>;
@@ -16,7 +24,29 @@ const Leaf = ({ attributes, children, leaf }) => {
 	if (leaf.underline) {
 		children = <u>{children}</u>;
 	}
-
+	// //  according to Text The custom properties in determine the style type
+	if (leaf.highlight) {
+		const coords = getSelectionCoords();
+		const yPos = coords.y;
+		if (!assignedColors[yPos]) {
+			assignedColors[yPos] = allColors.pop();
+		}
+		children = (
+			<span
+				className={`tag-highlight ${yPos}`}
+				style={{
+					backgroundColor: assignedColors[yPos],
+					opacity: '0.7',
+				}}>
+				{children}
+			</span>
+		);
+		console.log(yPos);
+		setTimeout(() => {
+			colorTags();
+		}, 200);
+		placeDiv(yPos, window.getSelection().toString());
+	}
 	return <span {...attributes}>{children}</span>;
 };
 
