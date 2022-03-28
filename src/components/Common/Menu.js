8ac246e0e95c55bc0ef0c579e93b977f75941982
Rajatm544 <rajatm544@gmail.com>
2022-03-28 19:33:06 +0530

    add the Menu element for the toolbar render

--- src/components/Common/Menu.js
+++ src/components/Common/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { cx, css } from '@emotion/css';
 
+// Slate Menu Element to display as Toolbar
 const Menu = React.forwardRef(({ className, ...props }, ref) => (
 	<div
 		{...props}
