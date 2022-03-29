8aab0d0659e94f7fa4b0a3e2442cb510a170797e
Rajatm544 <rajatm544@gmail.com>
2022-03-29 09:34:48 +0530

    change heading

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,7 @@ import '../styles/mainContainer.css';
 const MainContainer = () => {
 	return (
 		<main className='container'>
-			<h4 className='heading'>Rajat Interview</h4>
+			<h4 className='heading'>John Doe Interview</h4>
 			<SlateEditor />
 		</main>
 	);
