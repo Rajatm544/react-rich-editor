28f47650a2500ac0a515101862aea8d71dca395a
Rajatm544 <rajatm544@gmail.com>
2022-03-29 09:35:14 +0530

    change intial value and add comments

--- src/components/SlateEditor.js
+++ src/components/SlateEditor.js
@@ -139,10 +139,11 @@ const SlateEditor = () => {
 							}
 						}
 						const { selection } = editor;
+
 						// Default left/right behavior is unit:'character'.
 						// This fails to distinguish between two cursor positions, such as
 						// <inline>foo<cursor/></inline> vs <inline>foo</inline><cursor/>.
-						// Here we modify the behavior to unit:'offset'.
+						// Here the modified behavior unit is 'offset'.
 						// This lets the user step into and out of the inline without stepping over characters.
 						// You may wish to customize this further to only use unit:'offset' in specific cases.
 						if (selection && Range.isCollapsed(selection)) {
@@ -201,7 +202,7 @@ const initialValue = [
 	},
 	{
 		type: 'paragraph',
-		align: 'center',
+		align: 'left',
 		children: [{ text: 'Try it out for yourself!' }],
 	},
 ];
