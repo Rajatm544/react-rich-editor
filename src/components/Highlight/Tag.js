51df0dfb23984179eaf6600ceac277014a639b5d
Rajatm544 <rajatm544@gmail.com>
2022-03-30 11:26:50 +0530

    add comments to the useeffect in Tag component

--- src/components/Highlight/Tag.js
+++ src/components/Highlight/Tag.js
@@ -4,6 +4,8 @@ import '../../styles/editor.css';
 
 const Tag = ({ children, coords }) => {
 	const [yPos, setYPos] = useState(0);
+
+	// after y coordinates are passed in props, assign the tag colors and place the tag div
 	useEffect(() => {
 		if (coords.y) {
 			setYPos(coords.y);
@@ -15,9 +17,11 @@ const Tag = ({ children, coords }) => {
 
 	useEffect(() => {
 		if (yPos) {
+			// send the most recently selected text to place in the tag
 			placeDiv(yPos, window.getSelection().toString());
 		}
 	}, [yPos]);
+
 	return (
 		<span
 			style={{
